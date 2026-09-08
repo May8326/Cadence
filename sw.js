cb0a65584f3a89cf98b4c8066319dadcdb2b552f
Maëlys <164163912+May8326@users.noreply.github.com>
2026-09-08 17:06:34 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'cadence-pwa-v1';
+const APP = ['./', './cadence_pwa.html', './manifest.json', './icon.svg'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match('./cadence_pwa.html'))));
+});

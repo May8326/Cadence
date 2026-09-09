1cf10f29a59ce22398ec9c36483690022252986b
Maëlys <164163912+May8326@users.noreply.github.com>
2026-09-09 16:03:53 +0200

    Update cache version and add Google Calendar integration

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'cadence-pwa-v5'; // v2 -> v3 : force le renouvellement du cache
+const CACHE = 'cadence-pwa-v6'; // v2 -> v3 : force le renouvellement du cache
 
 const APP = [
   './',
   './index.html',
   './manifest.json',
-  './icon.svg','./integrations/todoist.js','./integrations/todoist-client.json'
+  './icon.svg','./integrations/todoist.js','./integrations/todoist-client.json','./integrations/google-calendar.js'
 ];
 
 self.addEventListener('install', event => {
